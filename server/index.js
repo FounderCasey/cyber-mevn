1ce41a881bfe6210176b9af659907ff976b58bbd
FounderCasey <caseywilcox@outlook.com>
2019-08-29 20:59:55 -0700

    Restored front end

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const posts = require('./routes/api/posts')
